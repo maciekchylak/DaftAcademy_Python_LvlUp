b6c6b526f4ca87b41847b7c1d430187b3c98a5db
Maciej Chylak <56119413+maciekchylak@users.noreply.github.com>
2021-05-04 23:37:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-from datetime import datetime
-
-from fastapi import FastAPI, Response, Request, Depends, HTTPException, Cookie
+from fastapi import FastAPI, Response, Request, Depends, status, HTTPException, Cookie
 from fastapi.responses import HTMLResponse, PlainTextResponse, JSONResponse, RedirectResponse
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from typing import Optional
-
-
+from datetime import datetime
+# import hashlib
 
 app = FastAPI()
-app.token1 = []
-app.token2 = []
+app.access_token_1 = [] #przerobić to na liste i powinno śmigać
+app.access_token_2 = []
 
 security = HTTPBasic()
 
 
 @app.get("/hello", response_class=HTMLResponse)
 def hello():
-    return f'<h1>Hello! Today date is {str(datetime.today().strftime("%Y-%m-%d"))} </h1>'
+    return '<h1>Hello! Today date is ' + str(datetime.today().strftime('%Y-%m-%d')) + '</h1>'
 
 
 @app.post("/login_session", status_code=201)
@@ -24,7 +22,10 @@ def login_session(*, response: Response, credentials: HTTPBasicCredentials = Dep
     if not (credentials.username == '4dm1n' and credentials.password == 'NotSoSecurePa$$'):
         raise HTTPException(status_code=401)
 
-    response.set_cookie(key="session_token", value="1234321")
+    new_access_token_1 = hash(datetime.now())
+    app.access_token_1.append(str(new_access_token_1))
+    while len(app.access_token_1) > 3: app.access_token_1.pop(0)
+    response.set_cookie(key="session_token", value=new_access_token_1)
     return
 
 
@@ -33,12 +34,15 @@ def login_token(*, response: Response, credentials: HTTPBasicCredentials = Depen
     if not (credentials.username == '4dm1n' and credentials.password == 'NotSoSecurePa$$'):
         raise HTTPException(status_code=401)
 
-    return {"token": "1234"}
+    new_access_token_2 = hash(datetime.now())
+    app.access_token_2.append(str(new_access_token_2))
+    while len(app.access_token_2) > 3: app.access_token_2.pop(0)
+    return {"token": new_access_token_2}
 
 
 @app.get("/welcome_session")
 def welcome_session(*, request: Request, session_token: str = Cookie(None)):
-    if not (str(session_token) in app.token1):
+    if not (str(session_token) in app.access_token_1):
         raise HTTPException(status_code=401)
     if str(request.query_params.get("format")) == "json":
         return JSONResponse(content={"message": "Welcome!"})
@@ -50,29 +54,29 @@ def welcome_session(*, request: Request, session_token: str = Cookie(None)):
 
 @app.get("/welcome_token")
 def welcome_token(format: Optional[str] = "", token: Optional[str] = ""):
-    if not (str(token) in app.token2):
-        raise HTTPException(status_code=401)
+    if not (str(token) in app.access_token_2):
+       raise HTTPException(status_code=401) 
     if format == "json":
         return JSONResponse(content={"message": "Welcome!"})
     elif format == "html":
         return HTMLResponse(content="<h1>Welcome!</h1>")
     else:
         return PlainTextResponse(content="Welcome!")
-
+    
 
 @app.delete("/logout_session")
 def logout_session(format: Optional[str] = "", session_token: str = Cookie(None)):
-    if not (str(session_token) in app.token1):
+    if not (str(session_token) in app.access_token_1):
         raise HTTPException(status_code=401)
-    while session_token in app.token1: app.token1.remove(session_token)
+    while session_token in app.access_token_1: app.access_token_1.remove(session_token)
     return RedirectResponse("/logged_out?format=" + format, status_code=303)
 
 
 @app.delete("/logout_token")
 def logout_token(token: Optional[str], format: Optional[str] = ""):
-    if not (str(token) in app.token2):
+    if not (str(token) in app.access_token_2):
         raise HTTPException(status_code=401)
-    while token in app.token2: app.token2.remove(token)
+    while token in app.access_token_2: app.access_token_2.remove(token)
     return RedirectResponse("/logged_out?format=" + format, status_code=303)
 
 
